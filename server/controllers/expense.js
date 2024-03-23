ae1224cb41a9536362384b3cfe07f69c566d3edd
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2024-03-23 13:40:44 +0530

    added transactions.

--- server/controllers/expense.js
+++ server/controllers/expense.js
@@ -1,17 +1,22 @@
 const Expense = require('../models/expense');
+const sequelize = require('../util/database');
 
 exports.addExpense = async (req, res, next) => {
+    const transaction = await sequelize.transaction();
+
     const { amount, description, category } = req.body;
     try {
-        const expense = await req.user.createExpense({ amount, description, category });
+        const expense = await req.user.createExpense({ amount, description, category }, { transaction });
         req.user.totalExpense += Number(amount);
-        req.user.save();
+        await req.user.save({ transaction });
+        await transaction.commit();
         res.status(201).json({
             success: true,
             message: "Expense Added Successfully",
             data: expense.dataValues
         });
     } catch (err) {
+        await transaction.rollback();
         res.status(500).json({
             success: false,
             message: err
@@ -21,7 +26,7 @@ exports.addExpense = async (req, res, next) => {
 
 exports.getExpenses = async (req, res, next) => {
     try {
-        const data = await req.user.getExpenses({raw: true })
+        const data = await req.user.getExpenses({ raw: true })
         res.json({
             success: true,
             message: "All your expenses",
@@ -36,18 +41,22 @@ exports.getExpenses = async (req, res, next) => {
 };
 
 exports.deleteExpense = async (req, res, next) => {
+    const transaction = await sequelize.transaction();
+
     const id = req.params.id;
     try {
-        const data = await req.user.getExpenses({where: {id}})
+        const data = await req.user.getExpenses({ where: { id }, transaction })
         const expense = data[0].amount;
-        await data[0].destroy();
+        await data[0].destroy({ transaction });
         req.user.totalExpense -= expense;
-        req.user.save();
+        await req.user.save({ transaction });
+        await transaction.commit();
         res.json({
             success: true,
             message: "Expense deleted successfully"
         })
     } catch (err) {
+        transaction.rollback();
         res.status(500).json({
             success: false,
             message: err
